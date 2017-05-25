d4d93c3dee8eef9eb13f7aaf07ea0d921a9c2eef
Michael Kleinhenz <kleinhenz@redhat.com>
2017-05-26 01:05:43 +0200

    First setup with configuration and database.

--- themis.go
+++ themis.go
@@ -1,8 +1,69 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"themis/workitems"
+
+	"gopkg.in/mgo.v2"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+type Configuration struct {
+    servicePort int
+    databaseHost  string
+		databasePort int
+		databaseDatabase  string
+		databaseUser  string
+		databasePassword  string
+}
+
+func InitConfig() Configuration {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	
+	var configuration Configuration
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Config file not found..")
+	} else {
+		configuration.servicePort = viper.GetInt("service.port")
+
+		configuration.databaseHost = viper.GetString("database.host")
+		configuration.databasePort = viper.GetInt("database.port")
+		configuration.databaseDatabase = viper.GetString("database.database")
+		configuration.databaseUser = viper.GetString("database.user")
+		configuration.databasePassword = viper.GetString("database.password")
+
+		fmt.Printf("\nUsing configuration:\n service port = %d\n database host = %s\n", 
+			configuration.servicePort, configuration.databaseHost)
+	}
+	return configuration
+}
+
+func ConnectToDatabase(configuration Configuration) *mgo.Database {
+	session, err := mgo.DialWithInfo(&mgo.DialInfo {
+		Addrs:    []string { configuration.databaseHost },
+		Username: configuration.databaseUser,
+		Password: configuration.databasePassword,
+		Database: configuration.databaseDatabase,
+	})
+	if err != nil {
+		panic(err)
+	}
+	//defer session.Close()
+	fmt.Printf("Connected to database at %v!\n", session.LiveServers())
+	return session.DB(configuration.databaseDatabase)
+}
 
 func main() {
+	configuration := InitConfig()
+	database := ConnectToDatabase(configuration)
+	workitems.WriteToDatabase(database)
+	workItem := workitems.ReadFromDatabase(database, "newId0")
+	fmt.Printf("WorkItem data: %s %s", workItem.Id, workItem.Type)
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
--- workitems/workitems.go
+++ workitems/workitems.go
@@ -1,7 +1,63 @@
 package workitems;
 
+import (
+	"fmt"
 
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
 
+type WorkItem struct {
+    Id       string    `bson:"id"`
+		Type     string    `bson:"type"`
+}
+
+func NewWorkItem(id, typeStr string) WorkItem {
+	return WorkItem {
+		Id:   id,
+		Type: typeStr,
+	}
+}
+
+func WriteToDatabase(database *mgo.Database) {
+	workItem := NewWorkItem("newId0", "newType0");
+	coll := database.C("workitems")
+	if err := coll.Insert(workItem); err != nil {
+			panic(err)
+	}
+	fmt.Println("WorkItem inserted successfully!")
+}
+
+func ReadFromDatabase(database *mgo.Database, id string) WorkItem {
+	var workItem WorkItem
+	coll := database.C("workitems")
+	err := coll.Find(bson.M{"id": id}).One(&workItem)
+	if err != nil {
+			panic(err)
+	}
+	return workItem
+}
+
+func UpdateDatabase(database *mgo.Database) {
+	coll := database.C("workitems")
+	workItemId := bson.ObjectIdHex("55da804ea5b2a779329ceb8e") // FIXME
+	newValue := "Some Value"
+	update := bson.M{"$set": bson.M{"type": newValue}}
+	if err := coll.UpdateId(workItemId, update); err != nil {
+			panic(err)
+	}
+}
+
+func DeleteDatabase(database *mgo.Database) {
+	coll := database.C("workitems")
+	info, err := coll.RemoveAll(bson.M{"type": "some Type"})
+	if err != nil {
+			panic(err)
+	}
+	fmt.Printf("%d workItem(s) removed!\n", info.Removed)
+}
+
+/*
 type Vertex struct {
 	X, Y float64
 }
@@ -14,17 +70,4 @@ func main() {
 	v := Vertex{3, 4}
 	fmt.Println(v.Abs())
 }
-
-
-function NewMatrix(rows, cols int) *matrix {
-    m := new(matrix)
-    m.rows = rows
-    m.cols = cols
-    m.elems = make([]float, rows*cols)
-    return m
-}
-
-type Integer int;
-func (i *Integer) String() string {
-    return strconv.itoa(i)
-}
+*/
